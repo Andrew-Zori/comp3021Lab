6101f0bbf79ebfa486a6a072350f9eff4b9f07b9
Andrew-Zori <zzhouba@connect.ust.hk>
2022-02-12 18:15:57 +0800

    First message to commit

---
+++ src/comp3021/HelloGithub.java
@@ -0,0 +1,11 @@
+package comp3021;
+
+public class HelloGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+		System.out.println("Now I have another line");
+	}
+
+}
